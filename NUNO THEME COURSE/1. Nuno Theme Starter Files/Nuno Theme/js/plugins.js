bcf0a3d9bfd1b6120a0158b0f542dac4148733d0
unknown <dj.zaragoza@icloud.com>
2020-04-16 04:03:40 -0700

    styling navbar items

--- NUNO THEME COURSE/1. Nuno Theme Starter Files/Nuno Theme/js/plugins.js
+++ NUNO THEME COURSE/1. Nuno Theme Starter Files/Nuno Theme/js/plugins.js
@@ -1,24 +1,53 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
 
 function checkScroll() {
+  // @ts-ignore
   if ($(window).scrollTop() >= 300) {
+    // @ts-ignore
     $(".navbar").addClass("solid");
   } else {
+    // @ts-ignore
     $(".navbar").removeClass("solid");
   }
 }
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
+// @ts-ignore
 $(document).ready(function () {
   checkScroll();
+  // @ts-ignore
   $(window).scroll(checkScroll);
+  // @ts-ignore
   $(".navbar-toggler").click(function () {
+    // @ts-ignore
     if ($(window).scrollTop() <= 300) {
+      // @ts-ignore
       $("nav.navbar").toggleClass("solid-toggle");
     }
   });
 });
 /*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
+
+// @ts-ignore
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  // @ts-ignore
+  $(".navbar-toggler").addClass("collapsed");
+  // @ts-ignore
+  $("#navbarResponsive").removeClass("show");
+
+  // @ts-ignore
+  $("nav.navbar").removeClass("solid-toggle");
+
+  // @ts-ignore
+  $("html, body").animate(
+    {
+      // @ts-ignore
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    3000
+  );
+});
 
 /*========== BOUNCING DOWN ARROW ==========*/
 
@@ -36,12 +65,14 @@ $(document).ready(function () {
 
 /*========== WAYPOINTS ANIMATION DELAY ==========*/
 //Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
+// @ts-ignore
 $(function () {
   // a self calling function
   function onScrollInit(items, trigger) {
     // a custom made function
     items.each(function () {
       //for every element in items run function
+      // @ts-ignore
       var osElement = $(this), //set osElement to the current
         osAnimationClass = osElement.attr("data-animation"), //get value of attribute data-animation type
         osAnimationDelay = osElement.attr("data-delay"); //get value of attribute data-delay time
@@ -68,29 +99,36 @@ $(function () {
     });
   }
 
+  // @ts-ignore
   onScrollInit($(".os-animation")); //function call with only items
+  // @ts-ignore
   onScrollInit($(".staggered-animation"), $(".staggered-animation-container")); //function call with items and trigger
 });
 
 /*========== CONTACT FORM INPUT VALIDATION ==========*/
 //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
+// @ts-ignore
 $(function () {
   // init the validator
   // validator files are included in the download package
   // otherwise download from http://1000hz.github.io/bootstrap-validator
 
+  // @ts-ignore
   $("#contact-form").validator();
 
   // when the form is submitted
+  // @ts-ignore
   $("#contact-form").on("submit", function (e) {
     // if the validator does not prevent form submit
     if (!e.isDefaultPrevented()) {
       var url = "contact/contact.php";
 
       // POST values in the background the the script URL
+      // @ts-ignore
       $.ajax({
         type: "POST",
         url: url,
+        // @ts-ignore
         data: $(this).serialize(),
         success: function (data) {
           // data = JSON object that contact.php returns
@@ -110,8 +148,10 @@ $(function () {
           // If we have messageAlert and messageText
           if (messageAlert && messageText) {
             // inject the alert to .messages div in our form
+            // @ts-ignore
             $("#contact-form").find(".messages").html(alertBox);
             // empty the form
+            // @ts-ignore
             $("#contact-form")[0].reset();
           }
         },
